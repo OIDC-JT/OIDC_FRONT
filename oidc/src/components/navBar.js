f025c718f5908ee79b4438144d0db7ce1164e2c6
key1sam <seongminkim.dev@gmail.com>
2022-06-23 18:45:51 +0900

    컴포넌트 분할

---
+++ oidc/src/components/navBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+//import Layout from './component/layout';
+import './App.css';
+import './style.css'
+
+function Navbar() {
+    return (
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <a class="navbar-brand ps-3" href="/">MigHelper</a>
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                <li class="nav-item dropdown">
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#!">Settings</a></li>
+                        <li><a class="dropdown-item" href="#!">Activity Log</a></li>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li><a class="dropdown-item" href="#!">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
